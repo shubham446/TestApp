a4bd97c034bce808c1133346ceeded62a72c61c1
Shubham <shmraj75@gmail.com>
2020-06-22 13:02:18 -0700

    Section 5

--- TestAngApp-S/src/app/nav/nav.component.ts
+++ TestAngApp-S/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,29 +10,34 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('logged in successfully');
       console.log('logged in successfully');
     },
     error => {
+      this.alertify.error(error);
       console.log(error);
     }
     );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out')
+    this.alertify.message('logged out')
+    console.log('logged out');
   }
 
 }
